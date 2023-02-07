a839d28f59b67433af720e893c2ce06c364b0abf
Kolyansneg85 <69190840+Kolyansneg85@users.noreply.github.com>
2023-02-07 17:25:48 +0300

    Update station.rb

--- lesson3/station.rb
+++ lesson3/station.rb
@@ -1,3 +1,24 @@
 class Station
-  attr_accessor: trains
   
+    attr_accessor :trains, :name
+    def initialize(station_name)
+      @name = station_name
+      @trains = []
+    end
+
+    def arr(train)
+        self.trains << train
+    end
+
+    def dept(train)
+        self.departured.trains(train)
+    end
+
+    def trains_types_list (train_type)
+        @trains.count { |train| train.type == train_type }
+    end
+    
+    def train_types_by(type)
+        @trains.select { |train| train.type == train_type }
+      end
+end
