3b370f8c8326741298bfc4a170ff6fe896b3c3e5
Kolyansneg85 <69190840+Kolyansneg85@users.noreply.github.com>
2023-02-07 17:45:47 +0300

    Update route.rb

--- lesson3/route.rb
+++ lesson3/route.rb
@@ -4,4 +4,14 @@ class Route
     def initialize(first_station, last_station)
         @stations = [first_station, last_station]
       end
+
+      def add_station(station)
+        self.stations.insert(-2, station)
+        puts "Вы успешно добавили станцию #{station}"
+      end
+
+      def del_station(station)
+        self.stations.delete(station)
+        puts "Вы успешно удалили станцию #{station}"
+      end      
 end
