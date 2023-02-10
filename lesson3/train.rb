e43cc30d6d0f4b53d6c7a076ac92222659ad33d3
Kolyansneg85 <69190840+Kolyansneg85@users.noreply.github.com>
2023-02-10 16:08:14 +0300

    Add legacy class Route
    Добавил вагоны, наследование маршрута

--- lesson3/train.rb
+++ lesson3/train.rb
@@ -17,3 +17,32 @@ class Train
     def stop
         self.speed = 0
     end
+
+    def route = (route)
+        @route = route
+        @station = route.stations.first
+    end
+
+    def go_forward
+        station_num = @route.stations.index(@station)
+        self.station = @route.stations[num_station + 1]
+    end
+
+    def go_back
+        station_num = @route.stations.index(@station)
+        self.station = @route.stations[num_station - 1]
+    end
+
+    def carts_count
+        puts "#{self.carts}"
+    end
+
+    def plus_cart
+        self.carts += 1 if self.speed.zero?
+    end
+
+    def minus_cart
+        self.carts -= 1 if self.speed.zero? && self.carts > 0
+    end
+
+end
