986fd3569c353629f63b8db899f2567782ac7b32
Kolyansneg85 <69190840+Kolyansneg85@users.noreply.github.com>
2023-02-13 15:36:40 +0300

    Update route.rb

--- lesson3/route.rb
+++ lesson3/route.rb
@@ -7,15 +7,12 @@ class Route
 
       def add_station(station)
         self.stations.insert(-2, station)
-        puts "Вы успешно добавили станцию #{station}"
       end
 
       def del_station(station)
         self.stations.delete(station) unless [self.stations.first, self.stations.last].include?(station)
-        puts "Вы успешно удалили станцию #{station}"
       end    
       
       def station_list
-        puts "Станции от первой до конечной: #{self.stations}"
       end
 end
